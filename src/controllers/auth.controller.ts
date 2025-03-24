c212609971e3404f9566860fd29974eb58e60335
Vu Tien Duc <vutienduc26122002@gmail.com>
2025-03-25 00:38:04 +0700

    Implement Authentication

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -56,7 +56,7 @@ export class AuthController {
         const {email} = req.body;
         this.authService.sendOTP(email)
             .then(data => {
-                return res.status(200).json({message : SuccessMessages.COMPLETE_SIGNIN, data});
+                return res.status(200).json({message : null, data});
             })
             .catch(error => {
                 return res.status(500).json({ message: error.message || "Internal server error" });
